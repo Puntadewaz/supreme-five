a8c70f3d18bec4c99036a837f5e260f795780f40
Dewa <dewa.c@nawatech.co>
2024-07-13 18:58:37 +0700

    init commit

---
+++ resources/[qb]/qb-spawn/config.lua
@@ -0,0 +1,39 @@
+QB = {}
+
+QB.Spawns = {
+    -- ["legion"] = {
+    --     coords = vector4(195.17, -933.77, 29.7, 144.5),
+    --     location = "legion",
+    --     label = "Legion Square",
+    -- },
+
+    -- ["policedp"] = {
+    --     coords = vector4(428.23, -984.28, 29.76, 3.5),
+    --     location = "policedp",
+    --     label = "Police Department",
+    -- },
+
+    -- ["paleto"] = {
+    --     coords = vector4(80.35, 6424.12, 31.67, 45.5),
+    --     location = "paleto",
+    --     label = "Paleto Bay",
+    -- },
+
+    -- ["motel"] = {
+    --     coords = vector4(327.56, -205.08, 53.08, 163.5),
+    --     location = "motel",
+    --     label = "Motels",
+    -- },
+
+    ["pelabuhan"] = {
+        coords = vector4(-728.86, -1304.16, 5.1, 29.31),
+        location = "pelabuhan",
+        label = "Pelabuhan",
+    },
+
+    ["bandara"] = {
+        coords = vector4(-1037.06, -2736.98, 20.17, 328.09),
+        location = "bandara",
+        label = "Bandara",
+    },
+}
