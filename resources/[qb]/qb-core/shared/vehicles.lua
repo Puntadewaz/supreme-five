ce9b794590aba1e61e57aba5ea24710d5be8f612
Xivu909 <9xivu9@gmail.com>
2024-07-14 22:53:55 +0700

    add vehicle bmwems

--- resources/[qb]/qb-core/shared/vehicles.lua
+++ resources/[qb]/qb-core/shared/vehicles.lua
@@ -424,6 +424,7 @@ local Vehicles = {
     { model = 'urusperf23',      name = 'Urus',            brand = 'Lamborghini',          price = 2845000,  category = 'super',          type = 'automobile', shop = 'luxury' },
     { model = 'huayrar',      name = 'Huayra',            brand = 'Pagani',          price = 2845000,  category = 'super',          type = 'automobile', shop = 'luxury' },
     { model = 'nkscout',      name = 'Ambulance',            brand = 'Ford',          price = 2845000,  category = 'super',          type = 'automobile', shop = 'luxury' },
+    { model = 'bmwems',      name = 'Ambulance2',            brand = 'bmw',          price = 2845000,  category = 'super',          type = 'automobile', shop = 'luxury' },
     --- Motorcycles (8)
     { model = 'akuma',           name = 'Akuma',                         brand = 'Dinka',           price = 55000,   category = 'motorcycles',    type = 'bike',       shop = 'pdm' },
     { model = 'avarus',          name = 'Avarus',                        brand = 'LCC',             price = 20000,   category = 'motorcycles',    type = 'bike',       shop = 'pdm' },
