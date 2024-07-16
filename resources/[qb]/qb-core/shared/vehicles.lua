0420ef855275044cc6795aebc175deb38ad5767f
Xivu909 <9xivu9@gmail.com>
2024-07-17 00:07:31 +0700

    add vehicle wraithb

--- resources/[qb]/qb-core/shared/vehicles.lua
+++ resources/[qb]/qb-core/shared/vehicles.lua
@@ -427,6 +427,7 @@ local Vehicles = {
     { model = 'bmwems',      name = 'Ambulance2',            brand = 'bmw',          price = 2845000,  category = 'super',          type = 'automobile', shop = 'luxury' },
     { model = 'mclarenf1',      name = 'mclarenf1',            brand = 'mclaren',          price = 2845000,  category = 'super',          type = 'automobile', shop = 'luxury' },
     { model = 'fdnyambo',      name = 'EMS',            brand = 'AMBULU',          price = 2845000,  category = 'super',          type = 'automobile', shop = 'luxury' },
+    { model = 'wraithb',      name = 'RR WRAITH',            brand = 'RR',          price = 2845000,  category = 'super',          type = 'automobile', shop = 'luxury' },
     --- Motorcycles (8)
     { model = 'akuma',           name = 'Akuma',                         brand = 'Dinka',           price = 55000,   category = 'motorcycles',    type = 'bike',       shop = 'pdm' },
     { model = 'avarus',          name = 'Avarus',                        brand = 'LCC',             price = 20000,   category = 'motorcycles',    type = 'bike',       shop = 'pdm' },
