751460399f84424c62f77e2df708977fbd983cd2
Dewa <dewa.c@nawatech.co>
2024-07-19 15:55:46 +0700

    fixing bugs

--- resources/[mods]/pure-crafting/client/framework/esx/events.lua
+++
@@ -1,9 +0,0 @@
-if Config.framework ~= 'esx' then return end
-
-RegisterNetEvent('esx:playerLoaded', function()
-    TriggerServerEvent('pure-crafting:playerLoaded')
-end)
-
-RegisterNetEvent('esx:onPlayerLogout', function()
-    TriggerServerEvent('pure-crafting:playerUnloaded')
-end)
--- resources/[mods]/pure-crafting/client/framework/qbcore/events.lua
+++
@@ -1,9 +0,0 @@
-if Config.framework ~= 'qbcore' then return end
-
-RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
-    TriggerServerEvent('pure-crafting:playerLoaded')
-end)
-
-RegisterNetEvent('QBCore:Client:OnPlayerUnload', function()
-    TriggerServerEvent('pure-crafting:playerUnloaded')
-end)
